c30953753db53b648eb3dd80d70a0f9c1f0b2c2c
YongJin.Dev.PAPA <johnny@papamobility.com>
2022-08-30 17:39:31 +0900

    Initial Commit

---
+++ SwiftUITutorial/SwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITutorialApp.swift
+//  SwiftUITutorial
+//
+//  Created by YongJin on 2022/08/30.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
