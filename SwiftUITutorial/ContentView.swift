eada32217d81f395cb698d686a05a99c5b983910
YongJin.Dev.PAPA <johnny@papamobility.com>
2022-09-01 09:34:12 +0900

    [Feat.] Hstack과 VStack 뷰 생성, 텍스트 배치 테스트

--- SwiftUITutorial/ContentView.swift
+++ SwiftUITutorial/ContentView.swift
@@ -9,8 +9,52 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        
+        // 1번뷰
+        VStack(alignment: .leading) {
+            Text("불곡로 N번길")
+                .font(.title)
+                .fontWeight(.medium)
+                .foregroundColor(.green)
+            
+            HStack {
+                Text("분당구, 성남시, 경기도!")
+                // Spacer()
+                Text("HStack 사용중")
+                    .font(.subheadline)
+                    .bold()
+            }
             .padding()
+        }
+        
+        
+        // 2번 뷰
+        VStack {
+            Text("V스택 최상단")
+                .font(.title)
+                .foregroundColor(Color.pink)
+                .bold()
+            .padding( )
+            VStack { // V 스택 2번뷰
+                Text("V스택 상단 2번째")
+                    .font(.title)
+                    .foregroundColor(Color.blue)
+                    .bold()
+                .padding( )
+            
+                VStack {
+                    Text("V스택 상단 2번째의 내부")
+                        .font(.title)
+                        .bold()
+                    .padding()
+                }
+                Text("상단 2번째와 동일 블럭에 위치")
+            }
+            Text("최상단에 위치")
+                .bold()
+                .font(.body)
+                .padding()
+        }
     }
 }
 
