14ecb932ce786e18d312d60174067da2ca73f516
YongJinLeee <YJayLee.dev@gmail.com>
2024-08-29 19:06:20 +0900

    [Fixed] 앱 시작 진입지점 코드 변경 예시 (주석)

--- SwiftUITutorial/SwiftUITutorialApp.swift
+++ SwiftUITutorial/SwiftUITutorialApp.swift
@@ -4,14 +4,22 @@
 //
 //  Created by YongJin on 2022/08/30.
 //
-
+import ComposableArchitecture
 import SwiftUI
 
 @main
 struct SwiftUITutorialApp: App {
+    
+    static let store = Store(initialState: CounterFeature.State()) {
+        CounterFeature()
+            ._printChanges()
+    }
+
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+//            CounterView(store: SwiftUITutorialApp.store)
         }
     }
 }
