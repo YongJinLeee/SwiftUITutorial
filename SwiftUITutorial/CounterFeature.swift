4dd3167fda5e2f1f359fc6148955c8b15b8189fa
YongJinLeee <YJayLee.dev@gmail.com>
2024-08-29 14:53:50 +0900

    [TCA] Tutorial Stage 1 base File coding

---
+++ SwiftUITutorial/CounterFeature.swift
@@ -0,0 +1,22 @@
+//
+//  CounterFeature.swift
+//  SwiftUITutorial
+//
+//  Created by YongJin on 8/29/24.
+//
+
+import ComposableArchitecture
+
+@Reducer
+
+struct CounterFeature {
+
+    @ObservableState
+    struct state {
+
+    }
+
+    enum Action {
+
+    }
+}
