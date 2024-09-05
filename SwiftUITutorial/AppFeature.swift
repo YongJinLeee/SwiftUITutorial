56b26773927e2b0cc75e2fc1c6960d9abda77832
YongJinLeee <YJayLee.Dev@gmail.com>
2024-09-05 11:43:32 +0900

    [Fixed] 예시 - 불필요하게 2개로 나뉜 Store 기능

--- SwiftUITutorial/AppFeature.swift
+++ SwiftUITutorial/AppFeature.swift
@@ -6,13 +6,24 @@
 //
 
 import SwiftUI
+import ComposableArchitecture
 
 struct AppFeature: View {
+    
+    let store1 = StoreOf<CounterFeature>
+    let store2 = StoreOf<CounterFeature>
 
     var body: some View {
         TabView {
+            CounterView(store: store1)
+                .tabItem {
+                    Text("Counter 1")
+                }
 
+            CounterView(store: store2)
+                .tabItem { Text("Counter 2") }
         }
+
     }
 }
 
