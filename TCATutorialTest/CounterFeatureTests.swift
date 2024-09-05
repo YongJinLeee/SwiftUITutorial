285015b85e2fe169c49f267d5f269ad9ed446892
YongJinLeee <YJayLee.Dev@gmail.com>
2024-09-05 11:32:18 +0900

    [Fixed] 네트워크 연결 및 응답 Mock 테스트 등록

--- TCATutorialTest/CounterFeatureTests.swift
+++ TCATutorialTest/CounterFeatureTests.swift
@@ -57,15 +57,17 @@ final class CounterFeatureTests: XCTestCase {
 
         let store = await TestStore(initialState: CounterFeature.State()) {
             CounterFeature()
+        } withDependencies: {
+            $0.numberFact.fetch = { "\($0) is a good number." }
         }
 
         await store.send(.factButtonTapped) {
             $0.isLoading = true
         }
 
-        await store.send(.factButtonTapped) {
-
+        await store.receive(\.factResponse) {
             $0.isLoading = false
+            $0.fact = "0 is a good number."
         }
     }
 }
