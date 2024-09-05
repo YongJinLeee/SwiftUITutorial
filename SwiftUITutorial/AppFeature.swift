0d120a61845fd8c9ab7e387218610e3c557ab572
YongJinLeee <YJayLee.Dev@gmail.com>
2024-09-05 11:39:18 +0900

    [Feat.] AppFeature 파일 생성

---
+++ SwiftUITutorial/AppFeature.swift
@@ -0,0 +1,21 @@
+//
+//  AppFeature.swift
+//  SwiftUITutorial
+//
+//  Created by YongJin on 9/5/24.
+//
+
+import SwiftUI
+
+struct AppFeature: View {
+
+    var body: some View {
+        TabView {
+
+        }
+    }
+}
+
+#Preview {
+    AppFeature()
+}
